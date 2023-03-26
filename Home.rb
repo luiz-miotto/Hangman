1dbe26100d477e32500c920d38e62c820bb1669d
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2023-03-26 13:22:59 -0400

    first commit for home

---
+++ Home.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'json'
+require './models/game_session'
+require './ui/ui'
+array_of_words = ["hat","test","candy","tree","the"]
+
+
+
+ui = UI.new()
+
+
+
+
+
+#file = File.read('./lib/game1.json')
+#data_hash = JSON.parse(file)
+#p data_hash
+
+
+save_name = "game4"
+
+#game_two = GameSession.new()
+#game_two.set_word("cheese")
+#game_two.add_letter("V")
+#game_two.save_game("lumi_test1")
+
+
+
